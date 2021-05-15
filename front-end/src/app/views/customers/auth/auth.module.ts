a38c5e650fcecd4f77abf7632a648ef15b785683
Quang Dong <magenkyou0609@gmail.com>
2021-05-15 14:55:03 +0700

    Add auth module - Add forgot password, reset password. - Update authentication layout.

--- front-end/src/app/views/customers/auth/auth.module.ts
+++ front-end/src/app/views/customers/auth/auth.module.ts
@@ -4,11 +4,17 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { HomeLoginComponent } from './home-login/home-login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
     HomeLoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    ForgotPasswordComponent,
+    AuthLayoutComponent,
+    ResetPasswordComponent
   ],
   imports: [
     CommonModule,
